3d4f2e6c3d303529ad1ac6c14878248eb2e7d6b3
Kdaniel-prog <74417423+Kdaniel-prog@users.noreply.github.com>
2022-03-19 14:22:04 +0100

    Initial commit

---
+++ src/test/java/kiszel/daniel/temalabor/TemalaborApplicationTests.java
@@ -0,0 +1,13 @@
+package kiszel.daniel.temalabor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemalaborApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
